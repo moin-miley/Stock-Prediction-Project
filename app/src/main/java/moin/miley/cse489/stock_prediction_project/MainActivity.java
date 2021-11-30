8ef8eaff6513c73765bc053111387b28e3dd9f42
Nafisha-del <nafishamoin7@gmail.com>
2021-11-30 14:21:09 +0600

    The sidebar menu was updated.

--- app/src/main/java/moin/miley/cse489/stock_prediction_project/MainActivity.java
+++ app/src/main/java/moin/miley/cse489/stock_prediction_project/MainActivity.java
@@ -41,7 +41,8 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_profile, R.id.nav_shares, R.id.nav_slist,
+                R.id.nav_pred, R.id.nav_cc, R.id.nav_news, R.id.nav_about)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
---
+++ app/src/main/java/moin/miley/cse489/stock_prediction_project/ui/StockList/StockListViewModel.java
@@ -0,0 +1,19 @@
+package moin.miley.cse489.stock_prediction_project.ui.StockList;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class StockListViewModel extends ViewModel{
+
+    private MutableLiveData<String> mText;
+
+    public StockListViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
