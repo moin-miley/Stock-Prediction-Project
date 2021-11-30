8a8483fdcd5c181e6f412c19b5af2ab0da230fcd
Nafisha-del <nafishamoin7@gmail.com>
2021-11-30 13:46:22 +0600

    Initial commit

---
+++ app/src/main/java/moin/miley/cse489/stock_prediction_project/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package moin.miley.cse489.stock_prediction_project.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
