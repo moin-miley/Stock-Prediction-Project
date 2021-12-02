c362519fe120e17afbc2d46282922d429f62bebe
Nafisha-del <nafishamoin7@gmail.com>
2021-12-02 19:07:19 +0600

    After editing the fragment_home.xml and fragment_gallery.xml

--- app/src/main/java/moin/miley/cse489/stock_prediction_project/ui/gallery/GalleryFragment.java
+++ app/src/main/java/moin/miley/cse489/stock_prediction_project/ui/gallery/GalleryFragment.java
@@ -5,15 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import moin.miley.cse489.stock_prediction_project.R;
 import moin.miley.cse489.stock_prediction_project.databinding.FragmentGalleryBinding;
 
 public class GalleryFragment extends Fragment {
@@ -29,13 +25,13 @@ public class GalleryFragment extends Fragment {
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
+        /*final TextView textView = binding.textGallery;
         galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
         return root;
     }
 
