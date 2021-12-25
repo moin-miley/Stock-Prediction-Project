6457f147ce932c3d2e508882f06b8f8d0d588f0d
Nafisha-del <nafishamoin7@gmail.com>
2021-12-25 23:16:04 +0600

    Adding Webview to show different news pages

---
+++ app/src/main/java/moin/miley/cse489/stock_prediction_project/ui/News/NewsChannel1.java
@@ -0,0 +1,27 @@
+package moin.miley.cse489.stock_prediction_project.ui.News;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import moin.miley.cse489.stock_prediction_project.R;
+
+public class NewsChannel1 extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.news_channel_1, container, false);
+
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        WebView myWebView = (WebView) getView().findViewById(R.id.webviewNew1);
+        myWebView.loadUrl("https://www.arthosuchak.com/");
+    }
+}
