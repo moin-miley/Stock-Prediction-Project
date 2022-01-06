5a0e3e6ea1bb48bcbf6fd4d61d39641ef3f7d7a4
Nafisha-del <nafishamoin7@gmail.com>
2022-01-06 19:56:58 +0600

    Finished version of News Fragment.

--- app/src/main/java/moin/miley/cse489/stock_prediction_project/ui/news/NewsFragment.java
+++ app/src/main/java/moin/miley/cse489/stock_prediction_project/ui/news/NewsFragment.java
@@ -17,6 +17,7 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -26,7 +27,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import moin.miley.cse489.stock_prediction_project.R;
 
 public class NewsFragment extends Fragment {
-    ImageView image1, image2, image3, image4, image5;
+    CardView cardView1, cardView2, cardView3, cardView4, cardView5;
     private ActionBar toolbar;
     @Nullable
     @Override
@@ -37,13 +38,13 @@ public class NewsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        image1 = (ImageView) getView().findViewById(R.id.new1);
-        image2 = (ImageView) getView().findViewById(R.id.new2);
-        image3 = (ImageView) getView().findViewById(R.id.new3);
-        image4 = (ImageView) getView().findViewById(R.id.new4);
-        image5 = (ImageView) getView().findViewById(R.id.new5);
+        cardView1 = (CardView) getView().findViewById(R.id.cardview1);
+        cardView2 = (CardView) getView().findViewById(R.id.cardview2);
+        cardView3 = (CardView) getView().findViewById(R.id.cardview3);
+        cardView4 = (CardView) getView().findViewById(R.id.cardview4);
+        cardView5 = (CardView) getView().findViewById(R.id.cardview5);
 
-        image1.setOnClickListener(new View.OnClickListener() {
+        cardView1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Fragment someFragment = new NewsChannel1();
@@ -51,9 +52,14 @@ public class NewsFragment extends Fragment {
                 transaction.replace(R.id.relativeLayoutNews, someFragment ); // give your fragment container id in first parameter
                 transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
                 transaction.commit();
+                getView().findViewById(R.id.cardview1).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview2).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview3).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview4).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview5).setVisibility(View.INVISIBLE);
             }
         });
-        image2.setOnClickListener(new View.OnClickListener() {
+        cardView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Fragment someFragment = new NewsChannel2();
@@ -61,6 +67,56 @@ public class NewsFragment extends Fragment {
                 transaction.replace(R.id.relativeLayoutNews, someFragment ); // give your fragment container id in first parameter
                 transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
                 transaction.commit();
+                getView().findViewById(R.id.cardview1).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview2).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview3).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview4).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview5).setVisibility(View.INVISIBLE);
+            }
+        });
+        cardView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment someFragment = new NewsChannel3();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.relativeLayoutNews, someFragment ); // give your fragment container id in first parameter
+                transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
+                transaction.commit();
+                getView().findViewById(R.id.cardview1).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview2).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview3).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview4).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview5).setVisibility(View.INVISIBLE);
+            }
+        });
+        cardView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment someFragment = new NewsChannel4();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.relativeLayoutNews, someFragment ); // give your fragment container id in first parameter
+                transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
+                transaction.commit();
+                getView().findViewById(R.id.cardview1).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview2).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview3).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview4).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview5).setVisibility(View.INVISIBLE);
+            }
+        });
+        cardView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment someFragment = new NewsChannel5();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.relativeLayoutNews, someFragment ); // give your fragment container id in first parameter
+                transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
+                transaction.commit();
+                getView().findViewById(R.id.cardview1).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview2).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview3).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview4).setVisibility(View.INVISIBLE);
+                getView().findViewById(R.id.cardview5).setVisibility(View.INVISIBLE);
             }
         });
     }
