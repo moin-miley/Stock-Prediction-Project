c50f21712016d2e34169ef0d503a9f3b5242f173
Nafisha-del <nafishamoin7@gmail.com>
2022-01-06 21:23:11 +0600

    Finished version of Currency Convertion feature.

--- app/src/main/java/moin/miley/cse489/stock_prediction_project/ui/CurrencyConvert/CurrencyConvertFragment.java
+++ app/src/main/java/moin/miley/cse489/stock_prediction_project/ui/CurrencyConvert/CurrencyConvertFragment.java
@@ -1,5 +1,7 @@
 package moin.miley.cse489.stock_prediction_project.ui.CurrencyConvert;
 
+import static java.lang.Double.parseDouble;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -21,43 +23,186 @@ import moin.miley.cse489.stock_prediction_project.R;
 
 public class CurrencyConvertFragment extends Fragment {
     EditText enterAmount;
+    TextView message;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_cc, container, false);
+        View myInflatedView = inflater.inflate(R.layout.fragment_cc, container, false);
+        return myInflatedView;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        enterAmount = getView().findViewById(R.id.enterAmount);
-        TextView message = getView().findViewById(R.id.output);
         Spinner spinner1 = getView().findViewById(R.id.spinner1_cc);
         Spinner spinner2 = getView().findViewById(R.id.spinner2_cc);
         Button button = getView().findViewById(R.id.convertButton);
 
-
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity().getBaseContext(), R.array.currency, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(adapter);
+        String text = spinner1.getSelectedItem().toString();
+        System.out.println(text);
+
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(getActivity().getBaseContext(), R.array.currency, android.R.layout.simple_spinner_item);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(adapter2);
-
-        int spinner_pos = spinner1.getSelectedItemPosition();
-        String[] size_values = getResources().getStringArray(R.array.currency);
-        int size = Integer.valueOf(size_values[spinner_pos]);
+        String text2 = spinner2.getSelectedItem().toString();
+        System.out.println(text2);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Double tot;
-                Double amt = Double.parseDouble(enterAmount.getText().toString());
-
-                if(spinner1.getSelectedItem().toString() == "Taka" && spinner2.getSelectedItem().toString() == "USD")
+                enterAmount = getActivity().findViewById(R.id.enterAmount);
+                Double amt = parseDouble(enterAmount.getText().toString());
+                message = getActivity().findViewById(R.id.output);
+                message.setText("Null");
+                int spinner_pos1 = spinner1.getSelectedItemPosition();
+                int spinner_pos2 = spinner2.getSelectedItemPosition();
+                if(spinner_pos1==0 && spinner_pos2==0)
+                {
+                    String test = "The value is the same.";
+                    message.setText(test);
+                }
+                if(spinner_pos1==0 && spinner_pos2==1)
                 {
                     tot = amt * 0.012;
-                    String test = amt + " " + spinner1.getSelectedItem().toString() + " to " + spinner2.getSelectedItem().toString() + " is" + tot;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==0 && spinner_pos2==2)
+                {
+                    tot = amt * 0.010;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==0 && spinner_pos2==3)
+                {
+                    tot = amt * 1.35;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==0 && spinner_pos2==4)
+                {
+                    tot = amt * 0.87;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==1 && spinner_pos2==0)
+                {
+                    tot = amt * 85.95;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==1 && spinner_pos2==1)
+                {
+                    String test = "The value is the same.";
+                    message.setText(test);
+                }
+                if(spinner_pos1==1 && spinner_pos2==2)
+                {
+                    tot = amt * 0.88;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==1 && spinner_pos2==3)
+                {
+                    tot = amt * 115.71;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==1 && spinner_pos2==4)
+                {
+                    tot = amt * 74.47;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==2 && spinner_pos2==0)
+                {
+                    tot = amt * 97.24;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==2 && spinner_pos2==1)
+                {
+                    tot = amt * 1.13;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==2 && spinner_pos2==2)
+                {
+                    String test = "The value is the same.";
+                    message.setText(test);
+                }
+                if(spinner_pos1==2 && spinner_pos2==3)
+                {
+                    tot = amt * 130.89;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==2 && spinner_pos2==4)
+                {
+                    tot = amt * 84.20;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==3 && spinner_pos2==0)
+                {
+                    tot = amt * 0.74;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==3 && spinner_pos2==1)
+                {
+                    tot = amt * 0.0086;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==3 && spinner_pos2==2)
+                {
+                    tot = amt * 0.0076;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==3 && spinner_pos2==3)
+                {
+                    String test = "The value is the same.";
+                    message.setText(test);
+                }
+                if(spinner_pos1==3 && spinner_pos2==4)
+                {
+                    tot = amt * 0.64;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==4 && spinner_pos2==0)
+                {
+                    tot = amt * 1.16;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==4 && spinner_pos2==1)
+                {
+                    tot = amt * 0.013;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==4 && spinner_pos2==2)
+                {
+                    tot = amt * 0.012;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==4 && spinner_pos2==3)
+                {
+                    tot = amt * 1.55;
+                    String test = "The value is " + tot;
+                    message.setText(test);
+                }
+                if(spinner_pos1==4 && spinner_pos2==4)
+                {
+                    String test = "The value is the same.";
                     message.setText(test);
                 }
             }
